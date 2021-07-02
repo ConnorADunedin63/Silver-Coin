bbbcbf8635669b5b0e62d7dfe1c641008fbc4262
Connor <connora.wellington18@gmail.com>
2021-07-02 18:55:44 +1200

    Continued working on Home screen

--- code/pages/Home.tsx
+++ code/pages/Home.tsx
@@ -17,20 +17,20 @@
  import styles from './css/homeStyles';
 
 
-const Home: React.FC<any> = () => {
+const Home: React.FC<any> = ({navigation, route}) => {
   return (
     <View style={styles.container}>
       <View style={styles.titleContainer}>
         <Text style={styles.title}>Silver Coin</Text>
       </View>
       <View style={styles.buttonContainer}>
-        <Button title="Budget" color={"green"} onPress={() => {}} />
+        <Button title="Budget" color={"green"} onPress={() => {navigation.navigate("Budget")}} />
       </View>
       <View style={styles.buttonContainer}>
-        <Button title="Goals" color={"orange"} onPress={() => {}} />
+        <Button title="Goals" color={"orange"} onPress={() => {navigation.navigate("Goals")}} />
       </View>
       <View style={styles.buttonContainer}>
-        <Button title="Emergency Plan" color={"red"} onPress={() => {}} />
+        <Button title="Emergency Plan" color={"red"} onPress={() => {navigation.navigate("Planning")}} />
       </View>
     </View>
   )
